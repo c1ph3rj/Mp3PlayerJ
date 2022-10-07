1afebd3466ac604ebde422bdbd71580f848c8130
Jeeva Prakash <jeevarag1016@gmail.com>
2022-10-07 11:25:21 +0530

    Third push from office on 11:24AM 10/07/2022

--- app/src/main/java/com/c1ph3r/musicplayer/MainActivity.java
+++ app/src/main/java/com/c1ph3r/musicplayer/MainActivity.java
@@ -16,26 +16,27 @@ import com.google.android.material.button.MaterialButton;
 import com.google.android.material.button.MaterialButtonToggleGroup;
 
 public class MainActivity extends AppCompatActivity {
-    Runnable runnable;
-    Handler handler;
     MediaPlayer mediaPlayer;
     MaterialButton playPauseBtn;
     int songNo = 0;
     AnimationDrawable animate;
     SeekBar seekBar;
     MaterialButtonToggleGroup playPause;
-    TextView textView;
+    TextView textView, userTime, trackTime;
     String[] songName = {"song_one","song_two","song_three"};
+    Thread updateSeekBar;
     ImageButton previous, next;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        seekBar = findViewById(R.id.seekBar);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
         playPause = findViewById(R.id.playPause);
         playPauseBtn = findViewById(R.id.PlayPauseBtn);
         textView = findViewById(R.id.SongName);
+        userTime = findViewById(R.id.userTime);
+        trackTime = findViewById(R.id.trackTime);
         next = findViewById(R.id.nextSong);
         previous = findViewById(R.id.previousSong);
         View gradientTheme = findViewById(R.id.gradientTheme);
@@ -44,14 +45,46 @@ public class MainActivity extends AppCompatActivity {
         animate = (AnimationDrawable) gradientTheme.getBackground();
         animate.setExitFadeDuration(4000);
         animate.setEnterFadeDuration(2000);
-
-        runnable = new Runnable() {
+        trackTime.setText(updateTime(mediaPlayer.getDuration()));
+        updateSeekBar = new Thread(){
             @Override
             public void run() {
-                seekBar.setProgress(mediaPlayer.getCurrentPosition());
-                handler.postDelayed(this, 500);
+                int totalDuration = mediaPlayer.getDuration();
+                int currentPosition = 0;
+                while(currentPosition<totalDuration){
+                    currentPosition = mediaPlayer.getCurrentPosition();
+                    seekBar.setProgress(currentPosition);
+                }
+                super.run();
             }
         };
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                mediaPlayer.seekTo(seekBar.getProgress());
+                userTime.setText(updateTime(mediaPlayer.getCurrentPosition()));
+            }
+        });
+        seekBar.setMax(mediaPlayer.getDuration());
+        updateSeekBar.start();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                userTime.setText(updateTime(mediaPlayer.getCurrentPosition()));
+                handler.postDelayed(this,500);
+            }
+        }, 500);
 
         playPause.addOnButtonCheckedListener(new MaterialButtonToggleGroup.OnButtonCheckedListener() {
             @SuppressLint({"SetTextI18n", "UseCompatLoadingForDrawables"})
@@ -60,23 +93,22 @@ public class MainActivity extends AppCompatActivity {
                 if(isChecked) {
                     animate.start();
                     mediaPlayer.start();
+                    trackTime.setText(updateTime(mediaPlayer.getDuration()));
                     System.out.println("true");
                     playPauseBtn.setIconResource(R.drawable.pause);
+                    seekBar.setMax(mediaPlayer.getDuration());
                     mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
                         @Override
                         public void onCompletion(MediaPlayer mediaPlayer) {
                             next.performClick();
                         }
                     });
-                    seekBar.setMax(mediaPlayer.getDuration());
-                    handler.postDelayed(runnable, 0);
                 }
                 else {
                    mediaPlayer.pause();
                    animate.stop();
                     playPauseBtn.setIconResource(R.drawable.play_ic);
                     System.out.println("false");
-                    handler.removeCallbacks(runnable);
                 }
             }
         });
@@ -84,15 +116,14 @@ public class MainActivity extends AppCompatActivity {
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(!mediaPlayer.isPlaying()){
-                    playPauseBtn.toggle();
-                }else{
-                    if(songNo <songName.length-1)
+
+                    if(songNo <songName.length-1){
                         songNo+=1;
+                    }
                     else if(songNo==songName.length-1)
                         songNo = 0;
                     setSong();
-                }
+
 
             }
         });
@@ -110,18 +141,22 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
     }
 
 
     @SuppressLint("NewApi")
     public void setSong(){
         if(mediaPlayer.isPlaying()){
+            seekBar.setProgress(0);
             mediaPlayer.stop();
         }
         textView.setText(songName[songNo]);
         mediaPlayer = MediaPlayer.create(MainActivity.this,getResources().getIdentifier(songName[songNo],"raw",getPackageName()));
+        playPauseBtn.setChecked(true);
         mediaPlayer.start();
+        seekBar.setProgress(0);
+        trackTime.setText(updateTime(mediaPlayer.getDuration()));
+        seekBar.setMax(mediaPlayer.getDuration());
         mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer mediaPlayer) {
@@ -129,5 +164,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    public String updateTime(int duration){
+        String time = "";
+        int min = (duration/1000/60);
+        int sec = (duration/1000%60);
+        time = min + ":" + ((sec<10)? "0" + sec:sec);
+        return time;
+    }
+
 
 }
